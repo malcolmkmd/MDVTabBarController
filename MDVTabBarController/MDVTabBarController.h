ba78afa0f78b2fa33694146b1c9fdbe400d36d72
Malcolm <malcolm@mdevsa.com>
2018-05-15 21:26:28 +0200

    Initial Commit

---
+++ MDVTabBarController/MDVTabBarController.h
@@ -0,0 +1,19 @@
+//
+//  MDVTabBarController.h
+//  MDVTabBarController
+//
+//  Created by Malcolm Kumwenda on 2018/05/15.
+//  Copyright © 2018 Malcolm Kumwenda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MDVTabBarController.
+FOUNDATION_EXPORT double MDVTabBarControllerVersionNumber;
+
+//! Project version string for MDVTabBarController.
+FOUNDATION_EXPORT const unsigned char MDVTabBarControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDVTabBarController/PublicHeader.h>
+
+
